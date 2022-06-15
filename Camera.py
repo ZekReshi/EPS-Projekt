8bcc991f933c723b92c3a3ee6942211f3e70f36f
Schwarcz Florian <florian.schwarcz@gmx.at>
2022-06-15 13:36:56 +0200

    Worked on blue lights detection

--- Camera.py
+++ Camera.py
@@ -1,3 +1,4 @@
+from typing import Dict, List
 import cv2 as cv
 import numpy as np
 
@@ -6,44 +7,64 @@ import Publish
 
 class Camera:
     cap: cv.VideoCapture
+    lights: Dict[int, List]
 
     def __init__(self):
         self.cap = cv.VideoCapture('evl4.mp4')
         self.last = None
+        self.processed_image = None
+        self.lights = {}
+        for i in range(10):
+            self.lights[i] = []
 
-    def take(self):
-        ret, img = self.cap.read()
+    def process_picture(self):
+        ret, self.img = self.cap.read()
 
         if ret:
-            blur = cv.GaussianBlur(img, (15, 15), 0)
+            for i in range(9):
+                self.lights[9 - i] = self.lights[8 - i]
+
+            blur = cv.GaussianBlur(self.img, (15, 15), 0)
             if self.last is not None:
                 gray = cv.cvtColor(blur, cv.COLOR_BGR2GRAY)
                 diff = cv.absdiff(gray, self.last)
                 mask = cv.inRange(diff, 127, 255)
                 blur = cv.bitwise_and(blur, blur, mask=mask)
+                self.processed_image = cv.cvtColor(blur, cv.COLOR_BGR2HSV)
                 self.last = gray
-
-                hsv = cv.cvtColor(blur, cv.COLOR_BGR2HSV)
-                lower_blue = np.array([95, 64, 127])
-                upper_blue = np.array([130, 255, 255])
-
-                mask = cv.inRange(hsv, lower_blue, upper_blue)
-
-                contours, _ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-                big_contours = []
-                for contour in contours:
-                    area = cv.contourArea(contour)
-                    if 0 < area < 255:
-                        big_contours.append(contour)
-                cv.drawContours(img, big_contours, -1, (0, 0, 255), cv.FILLED)
-
-                return img
             else:
                 self.last = cv.cvtColor(blur, cv.COLOR_BGR2GRAY)
 
-                return img
-
         return None
+
+    def detect_blue_lights(self):
+        self.process_picture()
+
+        if self.processed_image is None:
+            return None
+
+        lower_blue = np.array([95, 64, 127])
+        upper_blue = np.array([130, 255, 255])
+
+        mask = cv.inRange(self.processed_image, lower_blue, upper_blue)
+
+        contours, _ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        contour_positions = []
+        for contour in contours:
+            area = cv.contourArea(contour)
+            if 0 < area < 255:
+                M = cv.moments(contour)
+                x = int(M['m10'] / M['m00'])
+                y = int(M['m01'] / M['m00'])
+                contour_positions.append((x, y))
+        self.lights[0] = contour_positions
+
+        for i in range(10):
+            positions = self.lights[i]
+            for position in positions:
+                cv.circle(self.img, position, 10, (0, 0, 255), cv.FILLED)
+
+        return self.img
 
 
 if __name__ == "__main__":
@@ -54,12 +75,12 @@ if __name__ == "__main__":
     wr = cv.VideoWriter('blue3.mp4', cv.VideoWriter_fourcc('m', 'p', '4', 'v'), 15,
                         (int(cam.cap.get(3)), int(cam.cap.get(4))))
     while True:
-        img = cam.take()
+        img = cam.detect_blue_lights()
         publisher.send_on() # for testing
         if img is None:
             continue#break
         wr.write(img)
-        cv.imshow("blue", cam.take())
+        cv.imshow("blue", img)
         #publisher.send_on()  # or send_off
 
         k = cv.waitKey(5)
