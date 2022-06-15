feec26bcf8a9c848c8cfebbe1899b7cc89ea8be9
Maximilian Wahl <mexx.wale@gmail.com>
2022-06-15 13:33:53 +0200

    debugmessenger now emulates sensor behaviour

--- DebugMessenger.py
+++ DebugMessenger.py
@@ -1,12 +1,23 @@
 import paho.mqtt.client as mqtt
 import vehicledetectionmessage_pb2
-
-
+import random
+import time
 if __name__ == '__main__':
-    proto_msg = vehicledetectionmessage_pb2.PBMessage()
-    proto_msg.control.action = vehicledetectionmessage_pb2.Action.OFF
-    print(proto_msg.SerializeToString())
+
     client = mqtt.Client()
     client.connect("iot.soft.uni-linz.ac.at", 1883, 60)
-    ret = client.publish("emergencyvehicledetection", proto_msg.SerializeToString())
-    client.disconnect()#publishclient.
+    try:
+        while True:
+            rand = random.choices([0,1],weights = (20,70), k =1)
+
+            proto_msg = vehicledetectionmessage_pb2.PBMessage()
+            proto_msg.control.action = rand[0]
+            print("Sending "+ vehicledetectionmessage_pb2.Action.Name(proto_msg.control.action))
+            ret = client.publish("emergencyvehicledetection", proto_msg.SerializeToString())
+            time.sleep(5)
+    except KeyboardInterrupt:
+        print('stopped')
+
+
+
+    client.disconnect()
